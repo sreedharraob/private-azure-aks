ac810d7900ec99327579c2d3d765c6d78808083d
vani-shrivastav <shriv40@gmail.com>
2020-03-02 15:51:21 +0000

    Update main.tf

--- kuebernetes_Azure/cosmos/main.tf
+++ kuebernetes_Azure/cosmos/main.tf
@@ -1,1 +1,31 @@
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
 
+resource "azurerm_cosmosdb_account" "db" {
+  name                = "tfex-cosmos-db-${random_integer.ri.result}"
+  location            = UK south
+  resource_group_name = azurerm_resource_group.rg.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+  enable_automatic_failover = true
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 10
+    max_staleness_prefix    = 200
+  }
+
+  geo_location {
+    location          = var.failover_location
+    failover_priority = 1
+  }
+
+  geo_location {
+    prefix            = "tfex-cosmos-db-${random_integer.ri.result}-customid"
+    location          = azurerm_resource_group.rg.location
+    failover_priority = 0
+  }
+}
